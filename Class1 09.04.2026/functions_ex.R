71d08d16a7a3e06dbc4a6eda9dc27c002f7cfca7
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2026-04-09 15:35:17 +0200

    Function exercise 09.04.2026

---
+++ Class1 09.04.2026/functions_ex.R
@@ -0,0 +1,30 @@
+summation = function(x) {
+  sum = 0;
+  
+  for(i in 1:x) {
+    sum = sum + i;
+  }
+  
+  return(sum)
+}
+
+print(summation(25))
+
+countdown = function(x) {
+  for(j in x:1) {
+    print(paste("Countdown:", j))
+  }
+}
+
+countdown(15)
+
+customFactorial = function(x) {
+  factorial = 1
+  for(k in 2:x) {
+    factorial = factorial * k;
+  }
+  
+  return(factorial)
+}
+
+print(factorial(6))
