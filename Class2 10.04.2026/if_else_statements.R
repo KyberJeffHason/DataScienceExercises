ffb2e487fe940f0f640b992ec34050d02bb57710
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2026-04-10 11:04:24 +0200

    if else statement example

---
+++ Class2 10.04.2026/if_else_statements.R
@@ -0,0 +1,11 @@
+Sys.setenv(LANG="en") # changes logs/error messages to English
+
+y=4
+
+if(y>4) {
+  print("bigger than 4")
+} else if(y==4) {
+  print("y is 4")
+} else {
+  print("y smaller than 4")
+}
