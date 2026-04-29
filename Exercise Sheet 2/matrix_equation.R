e0914dfec698976776c606157f28a1155d848989
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2026-04-29 13:02:45 +0200

    Matrix equation exercise sheet

---
+++ Exercise Sheet 2/matrix_equation.R
@@ -0,0 +1,45 @@
+# First linear equation
+# Open the brackets
+A = matrix(
+  c(4, 2, 1, 1), 
+  nrow = 2,   
+  ncol = 2,         
+  byrow = TRUE          
+)
+
+b = c(5,8)
+print(A)
+solve(A,b)
+
+# 2nd linear equation
+
+A = matrix(
+c(1, 1, 1,
+  2,-1,1,
+  1,2,-1), 
+nrow = 3,   
+ncol = 3,         
+byrow = TRUE          
+)
+
+b = c(6,3,2)
+print(A)
+print(b)
+solve(A,b)
+
+# 3rd linear equation
+
+A = matrix(
+  c(1, 1, 1, 1,
+    2, -1, 1, 2,
+    1, 2, -1, 1,
+    3, 1, 2, -1), 
+  nrow = 4,   
+  ncol = 4,         
+  byrow = TRUE          
+)
+
+b = c(10,11,6,7)
+print(A)
+print(b)
+solve(A,b)
